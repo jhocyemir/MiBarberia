138922b3f8003a1dd79d88e44f585703eee75279
Jhon f. Chaves m <55995217+jhocyemir@users.noreply.github.com>
2019-10-19 18:11:53 -0500

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <ImageBackground
-        source={require("./assets/splash2.jpg")}
+        source={require("./assets/splash2.jpg")} /*el archivo debe tener formato png,  cambiar splash2.jpg a splas2.png*/ 
         style={{
           width: "100%",
           height: "100%"
